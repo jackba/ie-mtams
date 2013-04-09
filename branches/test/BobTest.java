a50fdb91f6a80a043d2ab540f8574b06f8adc1b0
BwanaRM@gmail.com <BwanaRM@gmail.com@63432308-c13f-84d8-a548-e58419399302>
2013-04-09 14:28:24 +0000

    Just a test java class from netbeans

---
+++ branches/test/BobTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bobtest;
+
+/**
+ *
+ * @author badger
+ */
+public class BobTest {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World. This is Bob");
+    }
+}
