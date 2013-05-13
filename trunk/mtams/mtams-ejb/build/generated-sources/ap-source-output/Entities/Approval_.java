4d415e4f846cdc484aac3bc41804605d518d3a5f
BwanaRM <BwanaRM@63432308-c13f-84d8-a548-e58419399302>
2013-05-13 17:40:52 +0000

    full system

---
+++ trunk/mtams/mtams-ejb/build/generated-sources/ap-source-output/Entities/Approval_.java
@@ -0,0 +1,26 @@
+package Entities;
+
+import Entities.Account;
+import Entities.Application;
+import Entities.Attachement;
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-05-13T13:46:22")
+@StaticMetamodel(Approval.class)
+public class Approval_ { 
+
+    public static volatile SingularAttribute<Approval, Integer> sectionapproved;
+    public static volatile ListAttribute<Approval, Attachement> attachementList;
+    public static volatile SingularAttribute<Approval, Integer> sectionid;
+    public static volatile SingularAttribute<Approval, String> fromsection;
+    public static volatile SingularAttribute<Approval, Application> applicationIdapplication;
+    public static volatile SingularAttribute<Approval, Account> accountIdaccount;
+    public static volatile SingularAttribute<Approval, String> notes;
+    public static volatile SingularAttribute<Approval, Date> date;
+    public static volatile SingularAttribute<Approval, Integer> idapproval;
+
+}
---
+++ trunk/mtams/mtams-ejb/src/java/DataAccess/PermissionFacade.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataAccess;
+
+import Entities.Permission;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Badger
+ */
+@Stateless
+public class PermissionFacade extends AbstractFacade<Permission> implements PermissionFacadeLocal {
+    @PersistenceContext(unitName = "mtams-ejbPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public PermissionFacade() {
+        super(Permission.class);
+    }
+    
+}
