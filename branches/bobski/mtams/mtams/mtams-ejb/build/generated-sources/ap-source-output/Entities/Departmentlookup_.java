8e4e81d0444807887d84a136c803ae5c43af7af8
BwanaRM <BwanaRM@63432308-c13f-84d8-a548-e58419399302>
2013-05-16 10:00:41 +0000

    fixed createAccount.xhtml to use new database

--- branches/bobski/mtams/mtams/mtams-ejb/build/generated-sources/ap-source-output/Entities/Departmentlookup_.java
+++ branches/bobski/mtams/mtams/mtams-ejb/build/generated-sources/ap-source-output/Entities/Departmentlookup_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-05-13T13:46:21")
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-05-16T11:13:59")
 @StaticMetamodel(Departmentlookup.class)
 public class Departmentlookup_ { 
 
--- branches/bobski/mtams/mtams/mtams-war/src/java/ManagedBeans/SessionBean.java
+++ branches/bobski/mtams/mtams/mtams-war/src/java/ManagedBeans/SessionBean.java
@@ -4,7 +4,6 @@
  */
 package ManagedBeans;
 
-
 import Entities.Account;
 import ServiceLayer.LoginHandlerLocal;
 import java.io.Serializable;
@@ -15,24 +14,24 @@ import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
+
 ;
 
 /**
  *
  * @author Badger
  */
-@ManagedBean(name="sessionBean")
+@ManagedBean(name = "sessionBean")
 @SessionScoped
-public class SessionBean implements Serializable{
-    
+public class SessionBean implements Serializable {
+
     private String username;
     private String password;
     private HttpSession session;
     private Account user;
-    
     @EJB
     private LoginHandlerLocal handler;
-    
+
     /**
      * Creates a new instance of LoginBean
      */
@@ -54,87 +53,87 @@ public class SessionBean implements Serializable{
     public void setPassword(String password) {
         this.password = password;
     }
-    
-    public boolean checkActive(){
+
+    public boolean checkActive() {
         return FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("userID") != null;
     }
 
-    public boolean checkFirstTime(){
-        return (Boolean)FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("isFirst");
-    }
-    
-    public boolean checkNotFirstTime(){
-        return !((Boolean)FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("isFirst")).booleanValue();
+    public boolean checkFirstTime() {
+        return (Boolean) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("isFirst");
     }
 
-    public String greeting(){
-         String userN = (String)FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("username");
-         //String
-         if((HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false) != null){
-            return "Hello " + userN;
-         }else{
-             return "You are not logged in or have performed a wrong navigation. Please click on the link to take you to the login page";
-         }
+    public boolean checkNotFirstTime() {
+        return !((Boolean) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("isFirst")).booleanValue();
     }
-    
-    public void addDate(){
+
+    public String greeting() {
+        String userN = (String) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("username");
+        //String
+        if ((HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false) != null) {
+            return "Hello " + userN;
+        } else {
+            return "You are not logged in or have performed a wrong navigation. Please click on the link to take you to the login page";
+        }
+    }
+
+    public void addDate() {
         user.setDatelogin(new Date());
         handler.modifyAccount(user);
     }
-    
-    private void setSessionVariables(){
+
+    private void setSessionVariables() {
         FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("userID", user.getIdaccount());
-        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("username",user.getUsername());
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("username", user.getUsername());
     }
-    
-    public String validate(){
+
+    public String validate() {
         user = handler.authenticate(this.username, this.password);
-        if(user != null){
-           int roleNum = handler.getAccountRole(user); 
-           if(roleNum < 20){
-               setSessionVariables();
-               //session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
-               if(user.getDatelogin() == null){
-                   addDate();
-                   FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("isFirst",true);
-                   return "userHome";
-               }else{
-                   addDate();
-                   FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("isFirst",false);
-                   return "userHome";
-               }
-               //logError = false;
-               
-            }else if(roleNum < 30){
-               setSessionVariables();
-               addDate();
-               //session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
-               //logError = false;
-               return "adminHome";
-            } else{
-                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,"Login Error","Account has been deactivated. Please contact admin."));
+        if (user != null) {
+            int roleNum = handler.getAccountRole(user);
+            if (roleNum < 20) {
+                setSessionVariables();
+                //session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+                if (user.getDatelogin() == null) {
+                    addDate();
+                    FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("isFirst", true);
+                    return "userHome";
+                } else {
+                    addDate();
+                    FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("isFirst", false);
+                    return "userHome";
+                }
+                //logError = false;
+
+            } else if (roleNum < 30) {
+                setSessionVariables();
+                addDate();
+                //session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+                //logError = false;
+                return "adminHome";
+            } else {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Login Error", "Account has been deactivated. Please contact admin."));
                 return "login";
             }
-        }else{
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,"Login Error","Incorrect username/password combination"));
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Login Error", "Incorrect username/password combination"));
             return "login";
         }
 
     }
-    
+
     public String invalidate() {
         //HttpServletRequest request = (HttpServletRequest);
-         //session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+        //session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
         //HttpSession session = request.getSession(true);
-        try{
-            
-        //((HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false)).invalidate();
-        }catch (NullPointerException e){
-        }finally{
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("userID", null);
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("username", null);
+            ((HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false)).invalidate();
+        } catch (NullPointerException e) {
+        } finally {
             return "login";
         }
         //FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
-        
-    }
 
+    }
 }
