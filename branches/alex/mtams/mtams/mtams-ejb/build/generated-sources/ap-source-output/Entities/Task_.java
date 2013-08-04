2224f258a1168c3a4ccc8f562e1c9e1c1feaf27d
damasioalexandre89@gmail.com <damasioalexandre89@gmail.com@63432308-c13f-84d8-a548-e58419399302>
2013-08-04 15:52:48 +0000

    Working Super Admin

---
+++ branches/alex/mtams/mtams/mtams-ejb/build/generated-sources/ap-source-output/Entities/Task_.java
@@ -0,0 +1,18 @@
+package Entities;
+
+import Entities.Permission;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-08-03T20:41:29")
+@StaticMetamodel(Task.class)
+public class Task_ { 
+
+    public static volatile SingularAttribute<Task, String> task;
+    public static volatile SingularAttribute<Task, String> description;
+    public static volatile ListAttribute<Task, Permission> permissionList;
+    public static volatile SingularAttribute<Task, Integer> idtasks;
+
+}
---
+++ branches/alex/mtams/mtams/mtams-ejb/build/generated-sources/ap-source-output/Entities/Travel_.java
@@ -0,0 +1,26 @@
+package Entities;
+
+import Entities.Application;
+import Entities.Attachement;
+import Entities.Conference;
+import Entities.Itinerary;
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-08-03T20:41:29")
+@StaticMetamodel(Travel.class)
+public class Travel_ { 
+
+    public static volatile ListAttribute<Travel, Itinerary> itineraryList;
+    public static volatile SingularAttribute<Travel, Integer> idtravel;
+    public static volatile ListAttribute<Travel, Application> applicationList;
+    public static volatile ListAttribute<Travel, Attachement> attachementList;
+    public static volatile SingularAttribute<Travel, String> description;
+    public static volatile SingularAttribute<Travel, Conference> conferenceIdconference;
+    public static volatile SingularAttribute<Travel, Date> datereturn;
+    public static volatile SingularAttribute<Travel, Date> datedeparture;
+
+}
