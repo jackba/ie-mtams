a507a26e85eeb0e5a7a7c26dee014739d55d6ea2
damasioalexandre89@gmail.com <damasioalexandre89@gmail.com@63432308-c13f-84d8-a548-e58419399302>
2013-08-04 16:09:41 +0000

    Latest

---
+++ branches/alex/mtams/mtams/mtams-ejb/src/java/DataAccess/DepartmentFacade.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataAccess;
+
+import Entities.Department;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Sparda
+ */
+@Stateless
+public class DepartmentFacade extends AbstractFacade<Department> implements DepartmentFacadeLocal {
+    @PersistenceContext(unitName = "mtams-ejbPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public DepartmentFacade() {
+        super(Department.class);
+    }    
+}
