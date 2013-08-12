8b722ace1a862ccfaa0137788ae8e82aea51c96f
monkeybugbazooka@gmail.com <monkeybugbazooka@gmail.com@63432308-c13f-84d8-a548-e58419399302>
2013-08-12 06:18:11 +0000

    Pulling Down Trunk under smurf to implement Ajax

---
+++ mtams/mtams/mtams-ejb/build/generated-sources/ap-source-output/Entities/Leavelookup_.java
@@ -0,0 +1,13 @@
+package Entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2013-08-07T17:48:31")
+@StaticMetamodel(Leavelookup.class)
+public class Leavelookup_ { 
+
+    public static volatile SingularAttribute<Leavelookup, String> leavetype;
+
+}
---
+++ mtams/mtams/mtams-ejb/src/java/DataAccess/CountrylookupFacadeLocal.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataAccess;
+
+import Entities.Countrylookup;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Badger
+ */
+@Local
+public interface CountrylookupFacadeLocal {
+
+    void create(Countrylookup countrylookup);
+
+    void edit(Countrylookup countrylookup);
+
+    void remove(Countrylookup countrylookup);
+
+    Countrylookup find(Object id);
+
+    List<Countrylookup> findAll();
+
+    List<Countrylookup> findRange(int[] range);
+
+    int count();
+    
+}
