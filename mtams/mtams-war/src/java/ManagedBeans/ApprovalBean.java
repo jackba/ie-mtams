5fdada81472c611942ddbfd851fdfc4fab385bd8
BwanaRM <BwanaRM@63432308-c13f-84d8-a548-e58419399302>
2013-09-11 10:24:50 +0000

    commit for piet

--- mtams/mtams-war/src/java/ManagedBeans/ApprovalBean.java
+++ mtams/mtams-war/src/java/ManagedBeans/ApprovalBean.java
@@ -215,7 +215,7 @@ public class ApprovalBean implements Serializable {
             appr.setNotes(approvalComment);
             approvalHandler.persistApproval(appr);
             
-            int s = 1;
+            short s = 1;
             selectedAccQte.setSelected(s);
             selectedCarQte.setSelected(s);
             selectedFlgQte.setSelected(s);
