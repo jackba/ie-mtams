0f7c64fe438ba6f03e18acd02145b29c3a54e9d7
nelaaro@gmail.com <nelaaro@gmail.com@63432308-c13f-84d8-a548-e58419399302>
2013-09-17 13:56:55 +0000

    Added the messages table for send and formating messages. Added the entity, session and handler beans

---
+++ branches/megamerge/mtams/mtams-ejb/src/java/DataAccess/MessagesFacadeLocal.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataAccess;
+
+import Entities.Messages;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author aaron
+ */
+@Local
+public interface MessagesFacadeLocal {
+
+    void create(Messages messages);
+
+    void edit(Messages messages);
+
+    void remove(Messages messages);
+
+    Messages find(Object id);
+
+    List<Messages> findAll();
+
+    List<Messages> findRange(int[] range);
+
+    int count();
+    
+}
---
+++ branches/megamerge/mtams/mtams-ejb/src/java/Entities/Messages.java
@@ -0,0 +1,139 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author aaron
+ */
+@Entity
+@Table(name = "MESSAGES")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Messages.findAll", query = "SELECT m FROM Messages m"),
+    @NamedQuery(name = "Messages.findByIdmessage", query = "SELECT m FROM Messages m WHERE m.idmessage = :idmessage"),
+    @NamedQuery(name = "Messages.findByDescrition", query = "SELECT m FROM Messages m WHERE m.descrition = :descrition"),
+    @NamedQuery(name = "Messages.findByMessage", query = "SELECT m FROM Messages m WHERE m.message = :message")})
+public class Messages implements Serializable {
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "NAME")
+    private String name;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "SUBJECT")
+    private String subject;
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "IDMESSAGE")
+    private Integer idmessage;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "DESCRITION")
+    private String descrition;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 1000)
+    @Column(name = "MESSAGE")
+    private String message;
+
+    public Messages() {
+    }
+
+    public Messages(Integer idmessage) {
+        this.idmessage = idmessage;
+    }
+
+    public Messages(Integer idmessage, String descrition, String message) {
+        this.idmessage = idmessage;
+        this.descrition = descrition;
+        this.message = message;
+    }
+
+    public Integer getIdmessage() {
+        return idmessage;
+    }
+
+    public void setIdmessage(Integer idmessage) {
+        this.idmessage = idmessage;
+    }
+
+    public String getDescrition() {
+        return descrition;
+    }
+
+    public void setDescrition(String descrition) {
+        this.descrition = descrition;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idmessage != null ? idmessage.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Messages)) {
+            return false;
+        }
+        Messages other = (Messages) object;
+        if ((this.idmessage == null && other.idmessage != null) || (this.idmessage != null && !this.idmessage.equals(other.idmessage))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Entities.Messages[ idmessage=" + idmessage + " ]";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+    
+}
