8fd977b0a03d48b5adf6b27bc63d2916ce4ed247
riaanvorster2@gmail.com <riaanvorster2@gmail.com@63432308-c13f-84d8-a548-e58419399302>
2013-09-17 13:07:48 +0000

    testing of ApprovalHandler.java with id

--- mtams/mtams-ejb/src/java/ServiceLayer/ApprovalHandlerLocal.java
+++ mtams/mtams-ejb/src/java/ServiceLayer/ApprovalHandlerLocal.java
@@ -3,8 +3,15 @@
  * and open the template in the editor.
  */
 package ServiceLayer;
+
+//import Entities.Account;
+//import Entities.Application;
+import Entities.Application;
+import Entities.Finalcosting;
 import Entities.Approval;
 import java.util.List;
+//import Entities.Quotes;
+//import java.util.List;
 import javax.ejb.Local;
 
 /**
@@ -13,14 +20,23 @@ import javax.ejb.Local;
  */
 @Local
 public interface ApprovalHandlerLocal {
-
+    
+    //public void persist(Approval approval, Finalcosting finalcosting);
+    //public void persistApplication(Application application);
     public void persistApproval(Approval approval);
-
+    //public void persistFinalcosting(Finalcosting finalcosting);
+    
     public void updateApproval(Approval approval, Integer ApprovalID);
-
+           
+    //public void updateFinalcosting(Finalcosting finalcosting, Integer finalcostingID);
+    
     public Approval findApproval(Integer id);
-
     public List<Approval> findApprovalbyApplication(Integer id);
-
-    public List<Approval> findApprovalbyAccount(Integer id);
+    public List<Approval> findApprovalbyAccount(Integer id);  
+    //public Finalcosting findFinalcosting(Integer id);
+    //public Application findByApplication(Integer id);
+    //public Account findByAccount(Integer id);
+    //public Quotes findByQuotes(Integer id);
+    
+    public List<Application> allApp(Integer id);
 }
