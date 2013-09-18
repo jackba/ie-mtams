3430f00206b4f84f5ec72e47c681e23b1344a479
nelaaro@gmail.com <nelaaro@gmail.com@63432308-c13f-84d8-a548-e58419399302>
2013-09-18 15:58:22 +0000

    fixed issue with removal of  nbproject instead of nbproject/private

---
+++ mtams/mtams-ejb/build/generated-sources/ap-source-output/Entities/Accomodationquotes_.java
@@ -0,0 +1,28 @@
+package Entities;
+
+import Entities.Attachement;
+import Entities.Quotes;
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.CollectionAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2013-08-20T21:04:48")
+@StaticMetamodel(Accomodationquotes.class)
+public class Accomodationquotes_ { 
+
+    public static volatile SingularAttribute<Accomodationquotes, Date> datecheckin;
+    public static volatile SingularAttribute<Accomodationquotes, String> description;
+    public static volatile SingularAttribute<Accomodationquotes, String> accomodationprovider;
+    public static volatile SingularAttribute<Accomodationquotes, Double> quotecost;
+    public static volatile SingularAttribute<Accomodationquotes, Date> datecheckout;
+    public static volatile SingularAttribute<Accomodationquotes, String> quotesource;
+    public static volatile SingularAttribute<Accomodationquotes, Integer> idaccomodationquotes;
+    public static volatile SingularAttribute<Accomodationquotes, Quotes> quotesIdquotes;
+    public static volatile CollectionAttribute<Accomodationquotes, Attachement> attachementCollection;
+    public static volatile SingularAttribute<Accomodationquotes, String> country;
+    public static volatile SingularAttribute<Accomodationquotes, String> city;
+    public static volatile SingularAttribute<Accomodationquotes, String> currency;
+
+}
