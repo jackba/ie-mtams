2b3e4f0cebb00cae441084c4dfd929e502dba075
nelaaro@gmail.com <nelaaro@gmail.com@63432308-c13f-84d8-a548-e58419399302>
2013-09-26 06:14:31 +0000

    hardcoded finalcosting app and account ids for testing. Create now goes home to approval home CRC and currency include not completely working but are being used. done with Edit removed authorisation from edit, and view. Until I can fix the persistance update in the ejb handler.

--- branches/megamerge/mtams/mtams-ejb/src/java/ServiceLayer/FinalCostingHandler.java
+++ branches/megamerge/mtams/mtams-ejb/src/java/ServiceLayer/FinalCostingHandler.java
@@ -12,6 +12,8 @@ import DataAccess.QuotesFacadeLocal;
 import Entities.Finalcosting;
 import Entities.Quotes;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
@@ -34,6 +36,7 @@ public class FinalCostingHandler implements FinalCostingHandlerLocal {
     private QuotesFacadeLocal quoteDao;
     private Quotes quotes = new Quotes();
     private Finalcosting fcostingRef;
+    private static final Logger logger = Logger.getLogger(DataLookUpHandler.class.getName());
 
     @Override
     public void persistFinalcosting(Finalcosting finalcosting) {
@@ -49,10 +52,13 @@ public class FinalCostingHandler implements FinalCostingHandlerLocal {
         Finalcosting oldForm = null;
         List<Finalcosting> allFcostings = finalDao.findAll();
         for (Finalcosting eachfcost : allFcostings) {
+            logger.log(Level.INFO, "eachfcost.getIdfinalcosting(): {0}", eachfcost.getIdfinalcosting());
             if (eachfcost.getIdfinalcosting().equals(finalcostingID)) {
                 oldForm = eachfcost;
+                logger.log(Level.INFO, "oldForm: {0}", oldForm.getIdfinalcosting());
                 //move conferenceID of old conference entry to new entry.
                 newForm.setIdfinalcosting(oldForm.getIdfinalcosting());
+                logger.log(Level.INFO, "newForm: {0}", newForm.getIdfinalcosting());
                 finalDao.edit(newForm);
             }
         }
